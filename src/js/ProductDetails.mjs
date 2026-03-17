e885fb6363c006725d2473c64b94257ce9b326f3
aguer47 <maguer@byupathway.edu>
2026-03-17 16:33:14 +0300

    Style: Add blank line for better code formatting

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -17,6 +17,7 @@ export default class ProductDetails {
     document
       .getElementById('addToCart')
       .addEventListener('click', this.addProductToCart.bind(this));
+    
   }
 
   addProductToCart() {
